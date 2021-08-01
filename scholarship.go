cd010ca961d2932127848c8f5ed154194324e188
nickyangle <nicky.angle8@gmail.com>
2021-08-01 14:40:33 -0700

    Profiles
    todo documents
    
    structs
    
    access your user profile
    
    Rebased and squashed Profiles
    
    conflicts resolved

---
+++ scholarship.go
@@ -0,0 +1,9 @@
+package main
+
+type Scholarship struct {
+	Name        string `json:"name"`
+	DueDate     string `json:"due_date"`
+	Description string `json:"description"`
+	Link        string `json:"link"`
+	//tags array still needs to be created
+}
