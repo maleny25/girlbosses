5762c5e1c8fef5536fb951dcbaf2685e008e81b0
Maleny <malenyruiz123@gmail.com>
2021-08-02 11:05:49 -0700

    user & navigation

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -10,6 +11,10 @@ import (
 
 var nextId int = 0
 var users []User
+
+// map of username: password
+var userMap = make(map[string]string)
+var loggedinUser int
 
 func GetNextId() int {
 	value := nextId
@@ -21,6 +26,32 @@ func GetUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"list": users})
 }
 
+func UserLogin(c *gin.Context) {
+	var user User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	fmt.Println(user.Username)
+	if pass, work := userMap[user.Username]; work {
+		if pass == user.Password {
+			for index := range users {
+				if users[index].Username == user.Username {
+					loggedinUser = index
+					c.String(http.StatusOK, c.FullPath()+"/"+strconv.Itoa(index))
+					return
+				}
+			}
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid"})
+			return
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid password"})
+			return
+		}
+	}
+	c.JSON(http.StatusBadRequest, gin.H{"error": "invalid username", "user": user.Username})
+}
+
 func SignUpUser(c *gin.Context) {
 	var user User
 	if err := c.ShouldBindJSON(&user); err != nil {
@@ -29,6 +60,9 @@ func SignUpUser(c *gin.Context) {
 	}
 	user.Id = GetNextId()
 	users = append(users, user)
+	userMap[user.Username] = user.Password
+	loggedinUser = user.Id
+
 	c.String(http.StatusCreated, c.FullPath()+"/"+strconv.Itoa(user.Id))
 }
 
@@ -49,11 +83,13 @@ func DeleteUser(c *gin.Context) {
 
 func main() {
 	users = append(users, User{Id: GetNextId(), Username: "CodeHouse", Password: "7/31/2021"})
+	userMap["CodeHouse"] = "7/31/2021"
 
 	r := gin.Default()
 	r.Use(static.Serve("/", static.LocalFile("./vue-project/dist", false)))
 	r.GET("/api/users", GetUsers)
 	r.POST("/api/users", SignUpUser)
 	r.DELETE("/api/users/:id", DeleteUser)
+	r.POST("/api/users/login", UserLogin)
 	r.Run(":8090")
 }
---
+++ vue-project/src/pages/foo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Navigation/>
+        foo
+    </div>
+</template>
+
+<script>
+  import Navigation from "@/components/Navigation.vue";
+
+  export default {
+    components: {
+      Navigation,
+    },
+  };
+</script>
